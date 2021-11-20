5a6bbc1c1a35ab5f48ded9907345f74c2edfa092
Mukul Kumar <kumarmukul771@gmail.com>
2021-11-20 21:26:37 +0530

    Check for student

--- Api/User.js
+++ Api/User.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('../DB/User');
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../DB/User");
 const route = express.Router();
 
-route.post('/', async (req, res)=> {
-    const{email,password,subjectcode,subjectname} = req.body;
-    let user = {};
-    user.email = email;
-    user.password = password;
-    user.subjectcode = subjectcode;
-    user.subjectname = subjectname;
-    let usermodel = new User(user);
-    await usermodel.save();
-    res.json (usermodel);
-})
+route.post("/", async (req, res) => {
+  const { email, password, subjectcode, subjectname } = req.body;
+
+  let existingTeacher = await User.findOne({ subjectcode: subjectcode });
+  if (existingTeacher) {
+    res.send({ message: "Teacher for given subject code already exists" });
+    return;
+  }
+  let user = {};
+  user.email = email;
+  user.password = password;
+  user.subjectcode = subjectcode;
+  user.subjectname = subjectname;
+  let usermodel = new User(user);
+  await usermodel.save();
+  res.json(usermodel);
+});
 
 module.exports = route;
